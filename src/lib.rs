1e46953dc5bbc8d777fa755d4291c863251d3292
William Howell <wlm.howell@gmail.com>
2020-07-13 01:57:30 -0400

    Added DI so I can test input from stdin
    After some Googling around, I found this SO post about testing input from stdin: https://stackoverflow.com/questions/28370126/how-can-i-test-stdin-and-stdout, and I decided to rewrite the codebase a little to allow for this testing.
    
    More or less, all I've done here is convert all the functions inside the maybe module into methods on the prompto type.
    
    This has the added benefit of opening up my class to other objects that implement BufRead and Write, so I'm curious to see if I could, for instance, test these functions with a file.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 //!
 //! Simple, functional, monadic command-line prompts.
 //! Heavily inspired by `readMaybe` from Haskell.
+use std::io::{self, BufRead, Write};
 
 /// # SafeParsable
 ///
@@ -10,6 +11,11 @@
 pub trait SafeParsable: Sized + Copy + Default + std::str::FromStr {}
 
 impl<T> SafeParsable for T where T: Sized + Copy + Default + std::str::FromStr {}
+
+struct Prompto<R, W> {
+    reader: R,
+    writer: W,
+}
 
 pub mod result {
     //! # Result
@@ -185,143 +191,178 @@ pub mod maybe {
     //! If you need more fine-grained control over the errors, see the sibling result module.
     use crate::*;
 
-    /// Get a newline-terminated string from stdin,
-    /// returning `None` if `std::io::stdout.flush()` fails
-    /// or if `std::io::stdin().read_line()` fails.
-    ///
-    /// # Arguments
-    /// * `msg` – a message to display to the user.
-    ///
-    /// # Example
-    /// ```
-    /// use prompto::maybe::*;
-    /// let res = get_line("What's your name?");
-    /// match res {
-    ///     Some(s) => println!("Nice to meet you, {}!", s),
-    ///     None    => println!("I'm sorry!")
-    /// }
-    /// ```
-    ///
-    /// The Haskell spec for this function is:
-    /// ```hs
-    /// promptLine :: String -> IO String
-    /// promptLine msg = do
-    ///     putStr msg
-    ///     hFlush stdout
-    ///     getLine
-    /// ```
-    pub fn get_line(msg: &str) -> Option<String> {
-        use std::io::Write;
+    impl<R, W> Prompto<R, W>
+        where
+            R: BufRead,
+            W: Write,
+    {
+        /// Get a newline-terminated string from stdin,
+        /// returning `None` if `std::io::stdout.flush()` fails
+        /// or if `std::io::stdin().read_line()` fails.
+        ///
+        /// # Arguments
+        /// * `msg` – a message to display to the user.
+        ///
+        /// # Example
+        /// ```
+        /// use prompto::maybe::*;
+        ///
+        /// let mut prompto = Prompto {
+        ///     reader: std::io::stdin().lock(),
+        ///     writer: std::io::stdout()
+        /// };
+        ///
+        /// let res = prompto.get_line("What's your name?");
+        ///
+        /// match res {
+        ///     Some(s) => println!("Nice to meet you, {}!", s),
+        ///     None    => println!("I'm sorry!")
+        /// }
+        /// ```
+        ///
+        /// The Haskell spec for this function is:
+        /// ```hs
+        /// promptLine :: String -> IO String
+        /// promptLine msg = do
+        ///     putStr msg
+        ///     hFlush stdout
+        ///     getLine
+        /// ```
+        pub fn get_line(&mut self, msg: &str) -> Option<String> {
 
-        print!("{}", msg);
+            match write!(&mut self.writer, "{}", msg) {
+                Ok(()) => (),
+                Err(_) => return None
+            }
 
-        // Force output to stdout before reading from stdin
-        match std::io::stdout().flush() {
-            Ok(()) => (),
-            Err(_) => return None,
+            // Force output to stdout before reading from stdin
+            match self.writer.flush() {
+                Ok(()) => (),
+                Err(_) => return None,
+            }
+
+            let mut buffer: String = String::new();
+
+            match self.reader.read_line(&mut buffer) {
+                Ok(_) => (),
+                Err(_) => return None,
+            }
+
+            Some(buffer.trim_end().to_owned())
         }
 
-        let mut buffer: String = String::new();
-
-        match std::io::stdin().read_line(&mut buffer) {
-            Ok(_) => (),
-            Err(_) => return None,
+        /// Attempts to convert the contents of a string to a type
+        /// that implements `std::str::FromStr`.
+        /// Returns `None` if conversion failed.
+        /// More or less a wrapper around `parse`.
+        ///
+        /// # Arguments
+        /// * `arg` – string to attempt to convert.
+        ///
+        /// # Example
+        /// ```
+        /// use prompto::maybe::*;
+        ///
+        /// let mut prompto = Prompto {
+        ///     reader: std::io::stdin().lock(),
+        ///     writer: std::io::stdout()
+        /// };
+        ///
+        /// let res = prompto.read::<i32>("32").map(|x| x * 2).unwrap();
+        ///
+        /// println!("Value of res: {}.", res);
+        /// ```
+        ///
+        /// The Haskell spec for this function is:
+        /// ```hs
+        /// readMaybe :: Read a => String -> Maybe a
+        /// ```
+        pub fn read<T>(&mut self, arg: &str) -> Option<T>
+            where
+                T: std::str::FromStr,
+        {
+            match T::from_str(arg) {
+                Ok(res) => Some(res),
+                Err(_) => None,
+            }
         }
 
-        Some(buffer.trim_end().to_owned())
-    }
-
-    /// Attempts to convert the contents of a string to a type
-    /// that implements `std::str::FromStr`.
-    /// Returns `None` if conversion failed.
-    /// More or less a wrapper around `parse`.
-    ///
-    /// # Arguments
-    /// * `arg` – string to attempt to convert.
-    ///
-    /// # Example
-    /// ```
-    /// use prompto::maybe::*;
-    /// let res = read::<i32>("32").map(|x| x * 2).unwrap();
-    /// println!("Value of res: {}.", res);
-    /// ```
-    ///
-    /// The Haskell spec for this function is:
-    /// ```hs
-    /// readMaybe :: Read a => String -> Maybe a
-    /// ```
-    pub fn read<T>(arg: &str) -> Option<T>
-    where
-        T: std::str::FromStr,
-    {
-        match T::from_str(arg) {
-            Ok(res) => Some(res),
-            Err(_) => None,
+        /// Gets a value of type `T` from the user, where `T` defines a default value
+        /// and implements `std::str::FromStr`.
+        /// This function returns `None` if it is not able to parse the user's input into `T`.
+        ///
+        /// # Arguments
+        /// `msg` – a message to display to the user.
+        ///
+        /// # Example
+        /// ```
+        /// use prompto::maybe::*;
+        ///
+        /// let mut prompto = Prompto {
+        ///     reader: std::io::stdin().lock(),
+        ///     writer: std::io::stdout()
+        /// };
+        ///
+        /// let res = prompto.input::<i32>("Please enter a number: ");
+        ///
+        /// match res {
+        ///     Some(x) => println!("Got {}.", x),
+        ///     None => println!("Got invalid input!")
+        /// }
+        /// ```
+        ///
+        /// I designed this function as a type-safe analogue of Python's `input` function.
+        /// However, this function returns an Option because it has no way to validate
+        /// the user's input.
+        ///
+        /// The Haskell spec for this function is:
+        /// ```hs
+        /// getLine >>= pure . (Text.Read.readMaybe :: String -> Maybe Int)
+        /// ```
+        pub fn input<T>(&mut self, msg: &str) -> Option<T>
+            where
+                T: SafeParsable,
+        {
+            self.get_line(msg).and_then(|s| self.read::<T>(&s))
         }
-    }
 
-    /// Gets a value of type `T` from the user, where `T` defines a default value
-    /// and implements `std::str::FromStr`.
-    /// This function returns `None` if it is not able to parse the user's input into `T`.
-    ///
-    /// # Arguments
-    /// `msg` – a message to display to the user.
-    ///
-    /// # Example
-    /// ```
-    /// use prompto::maybe::*;
-    /// let res = input::<i32>("Please enter a number: ");
-    /// match res {
-    ///     Some(x) => println!("Got {}.", x),
-    ///     None => println!("Got invalid input!")
-    /// }
-    /// ```
-    ///
-    /// I designed this function as a type-safe analogue of Python's `input` function.
-    /// However, this function returns an Option because it has no way to validate
-    /// the user's input.
-    ///
-    /// The Haskell spec for this function is:
-    /// ```hs
-    /// getLine >>= pure . (Text.Read.readMaybe :: String -> Maybe Int)
-    /// ```
-    pub fn input<T>(msg: &str) -> Option<T>
-    where
-        T: SafeParsable,
-    {
-        get_line(msg).and_then(|s| read::<T>(&s))
-    }
+        /// Prompts the user for a value of type `T` and validates it against `validator`.
+        /// If input or validation fails, this function re-prompts the user.
+        ///
+        /// # Arguments
+        /// * `msg` – a message to display to the user.
+        /// * `validator` – a function which immutably borrows a single argument of type `T` and returns a `bool`.
+        ///
+        /// # Example
+        /// ```
+        /// use prompto::maybe::*;
+        ///
+        /// let mut prompto = Prompto {
+        ///     reader: std::io::stdin().lock(),
+        ///     writer: std::io::stdout()
+        /// };
+        ///
+        /// let res: u32 = prompto.prompt("Please enter a number between 1 and 100: ", |x| 1 <= x && x <= 100);
+        /// ```
+        pub fn prompt<T, F>(&mut self, msg: &str, validator: F) -> T
+            where
+                T: SafeParsable,
+                F: Fn(T) -> bool,
+        {
+            loop {
+                let res: T = match self.input::<T>(msg) {
+                    Some(val) => val,
+                    None => {
+                        println!("Invalid input. Please try again.");
+                        continue;
+                    }
+                };
 
-    /// Prompts the user for a value of type `T` and validates it against `validator`.
-    /// If input or validation fails, this function re-prompts the user.
-    ///
-    /// # Arguments
-    /// * `msg` – a message to display to the user.
-    /// * `validator` – a function which immutably borrows a single argument of type `T` and returns a `bool`.
-    ///
-    /// # Example
-    /// ```
-    /// use prompto::maybe::*;
-    /// let res: u32 = prompt("Please enter a number between 1 and 100: ", |x| 1 <= x && x <= 100);
-    /// ```
-    pub fn prompt<T, F>(msg: &str, validator: F) -> T
-    where
-        T: SafeParsable,
-        F: Fn(T) -> bool,
-    {
-        loop {
-            let res: T = match input::<T>(msg) {
-                Some(val) => val,
-                None => {
-                    println!("Invalid input. Please try again.");
-                    continue;
+                if validator(res) {
+                    break res;
+                } else {
+                    println!("Invalid input! Please try again.");
                 }
-            };
-
-            if validator(res) {
-                break res;
-            } else {
-                println!("Invalid input! Please try again.");
             }
         }
     }
@@ -364,25 +405,41 @@ mod tests {
 
     #[test]
     fn sanity_checks() {
+        let input = b"";
+        let mut output = Vec::new();
+
+        let mut prompto = Prompto {
+            reader: &input[..],
+            writer: &mut output
+        };
+
         // parse and from_str should always be equal for the same arguments.
         assert_eq!("32".parse::<i32>().unwrap(), i32::from_str("32").unwrap());
 
         // A string with a valid integer should always succeed.
-        assert!(maybe::read::<i32>("32").is_some());
+        assert!(prompto.read::<i32>("32").is_some());
 
         // A string with any number of non-numeric characters should never succeed,
         // even if any part of the string *could* be valid.
-        assert!(maybe::read::<i32>("56 fdfs θ gx二éfs sdf34ごν53 df3dfsd2").is_none());
+        assert!(prompto.read::<i32>("56 fdfs θ gx二éfs sdf34ごν53 df3dfsd2").is_none());
 
         // Implicit widening conversions are okay...
-        assert!(maybe::read::<f32>("32").is_some());
+        assert!(prompto.read::<f32>("32").is_some());
 
         // But truncating conversions are not!
-        assert!(maybe::read::<i32>("32.32").is_none());
+        assert!(prompto.read::<i32>("32.32").is_none());
     }
 
     #[test]
     fn composite_type_checks() {
+        let input = b"";
+        let mut output = Vec::new();
+
+        let mut prompto = Prompto {
+            reader: &input[..],
+            writer: &mut output
+        };
+
         // Read should behave the same way as calling parse or calling from_str directly on the type.
         let call_through_trait = RGB::from_str(r"#fa7268").unwrap()
             == RGB {
@@ -390,7 +447,7 @@ mod tests {
                 g: 114,
                 b: 104,
             };
-        let call_through_maybe = maybe::read::<RGB>(r"#fa7268").unwrap()
+        let call_through_maybe = prompto.read::<RGB>(r"#fa7268").unwrap()
             == RGB {
                 r: 250,
                 g: 114,
@@ -401,12 +458,20 @@ mod tests {
         // Caveat: read cannot catch all possible errors in this case;
         // for instance, if you have a multi-byte character in this string,
         // the compiler itself will error out!
-        assert!(maybe::read::<RGB>(r"gkhgkjyfa7jhkhjk268").is_none());
+        assert!(prompto.read::<RGB>(r"gkhgkjyfa7jhkhjk268").is_none());
     }
 
     #[test]
     fn chaining_checks() {
-        let res = maybe::read::<i32>("32").map(|x| x * 2).unwrap();
+        let input = b"";
+        let mut output = Vec::new();
+
+        let mut prompto = Prompto {
+            reader: &input[..],
+            writer: &mut output
+        };
+
+        let res = prompto.read::<i32>("32").map(|x| x * 2).unwrap();
 
         assert_eq!(res, 64);
     }
